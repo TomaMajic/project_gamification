f98d983194e9439064f2b6e3576159f635caf4f3
Toma Majic <toma.majic@fer.hr>
2016-11-09 15:11:50 +0100

    test

--- projekt_gamification/db/migrate/20161107123105_devise_create_users.rb
+++ projekt_gamification/db/migrate/20161107123105_devise_create_users.rb
@@ -19,6 +19,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       t.string   :current_sign_in_ip
       t.string   :last_sign_in_ip
 
+
       ## Confirmable
       # t.string   :confirmation_token
       # t.datetime :confirmed_at
