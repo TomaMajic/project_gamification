2ab88c60a0a08c809b0c962be87d21fb70d207a4
Tamara Milisa <Tamara@Tamaras-MacBook-Air.local>
2016-11-10 12:44:24 +0100

    ja sam debil

--- projekt_gamification/db/migrate/20161110091933_create_achievements.rb
+++ projekt_gamification/db/migrate/20161110091933_create_achievements.rb
@@ -2,12 +2,12 @@ class CreateAchievements < ActiveRecord::Migration[5.0]
   def change
     create_table :achievements do |t|
 
-    	t.boolean "first_star" false
-    	t.boolean "ten_stars" false
-    	t.boolean "complete_begginer" false
-    	t.boolean "complete_intermediate" false
-    	t.boolean "complete_expert" false
-    	t.boolean "collect_all_stars" false
+    	t.boolean "first_star" :default => false
+    	t.boolean "ten_stars" :default => false
+    	t.boolean "complete_begginer" :default => false
+    	t.boolean "complete_intermediate" :default => false
+    	t.boolean "complete_expert" :default => false
+    	t.boolean "collect_all_stars" :default => false
 
         t.timestamps
     end
