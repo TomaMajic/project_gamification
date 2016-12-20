a8793b5be3a6ae4061d3b122e67e18a612b0195c
Tamara Milisa <Tamara@Tamaras-MacBook-Air.local>
2016-12-20 13:28:16 +0100

    opet nezna

--- projekt_gamification/app/controllers/levels_controller.rb
+++ projekt_gamification/app/controllers/levels_controller.rb
@@ -1,14 +1,4 @@
 class LevelsController < ApplicationController
 
-  def begginer
-  	#@level = Level.find("1")
-  	#l_id = @level.id
-  	@question = Question.find("1")
-  end
 
-  def intermediate 
-  end
-
-  def expert
-  end
 end
