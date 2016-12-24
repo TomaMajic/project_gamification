b3e1b4eb9e35d74c877cfd6e1375f7972cf8a36d
Toma Majic <toma.majic@fer.hr>
2016-12-24 15:16:53 +0100

    nista

--- projekt_gamification/test/controllers/concept_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ConceptControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get concept_index_url
-    assert_response :success
-  end
-
-end
